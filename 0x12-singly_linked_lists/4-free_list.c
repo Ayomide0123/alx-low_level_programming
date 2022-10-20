4900acb396724a62d86f6e41de578e70d335ca22
Ayomide <oyetimehin31@gmail.com>
2022-10-20 21:35:47 +0100

    C_Singly_linked_lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - frees a list
+ * @head: head var
+ */
+void free_list(list_t *head)
+{
+	list_t *temporary;
+
+	while (head != NULL)
+	{
+		temporary = head->next;
+		free(head->str);
+		free(head);
+		head = temporary;
+	}
+}
