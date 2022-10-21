00cafc9d07b140997c27f51fd521658129b24415
Ayomide <64165980+Ayomide0123@users.noreply.github.com>
2022-10-21 08:44:23 +0100

    list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * list_len - finds the number of elements in a linked list
- * @h: pointer to the start of the linked list
- *
- * Return: number of elements in the linked list
+ * list_len - returns the no. of elements in the list_t list
+ * @h: Listed lists
+ * Return: A count
  */
-
 size_t list_len(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int i;
+	unsigned int count = 0;
 
-	tmp = h;
-	for (i = 0; tmp; i++)
-		tmp = tmp->next;
-	return (i);
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
